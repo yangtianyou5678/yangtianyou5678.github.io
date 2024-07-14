afe80e30c3821b5bb89584f33798730a601d1345
yangtianyou5678 <xiaoyanggedenewbing@outlook.com>
2024-07-14 08:41:06 +0000

    Site updated: 2024-07-14 08:41:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/14/hello-world/","2024/07/14/这是一篇新的博文/"];function toRandomPost(){
+var posts=["2024/07/14/hello-world/","2024/07/14/这是一篇新的博文/","2024/07/14/这是一篇新的博文2/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
